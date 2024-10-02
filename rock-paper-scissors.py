88a9eff4402b109cdbadceb83ed023c65cd596ab
Teigan Bronec <71047133+Tarkann01@users.noreply.github.com>
2024-10-02 08:25:43 -0700

    Update rock-paper-scissors.py
    Added user Input

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -6,12 +6,13 @@ def RockPaperSiser(X):
     user = ""
     num = math.ranint(1,3)
     # geting the rock paper or siser
-    
-    
-    
 
-    
-
+def userInp():
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissors")
+    userChoice = int(input("What is your choice?"))
+    return userChoice
 
 
 
