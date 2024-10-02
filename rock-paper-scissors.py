0df3a8d7abe4e36bbf20e9cb9cf7ccd665baf267
Teigan Bronec <71047133+Tarkann01@users.noreply.github.com>
2024-10-02 08:06:43 -0700

    Add files via upload

---
+++ rock-paper-scissors.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Rock-paper-scissors
+# Purpose:
+#
+# Author:      teiga
+#
+# Created:     02/10/2024
+# Copyright:   (c) teiga 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
