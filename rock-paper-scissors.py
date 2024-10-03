1718111d276935e57a00473d0db04fb739cdaadf
isaacHkBishop <isaacbishop8893@gmail.com>
2024-10-03 08:08:57 -0700

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,27 +1,27 @@
 import math
 import random
 
-def RockPaperSiser(X):
+def RockPaperSiser(x):
     # we nead to have the user pick one and then the computer pick one
-    num = math.ranint(1,3)
+    num = random.randint(1,3)
     # geting the rock paper or siser
     # rock 3
     # siser 2
     # paper 1
     if num == x:
         return("you chose the same as the computer")
-        #RockPaperSiser(userInp())
-    elif num == 1 && x == 3:
+        RockPaperSiser(userInp())
+    elif num == 1 and x == 3:
         return("The computer won beter luck next time")
-    elif num == 2 && x == 1:
+    elif num == 2 and x == 1:
         return("The computer won beter luck next time")
-    elif num == 3 && x == 2:
+    elif num == 3 and x == 2:
         return("The computer won beter luck next time")
-    elif x == 1 && num == 3:
+    elif x == 1 and num == 3:
         return("Congrats you won!!")
-    elif x == 2 && num == 1:
+    elif x == 2 and num == 1:
         return("Congrats you won!!")
-    elif x == 3 &&  num == 2:
+    elif x == 3 and  num == 2:
         return("Congrats you won!!")
     
 def userInp():
