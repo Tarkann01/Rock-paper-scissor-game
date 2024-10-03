17e1a0b90c366c88ae5377ace6ba0f9bb8a9e2af
Teigan Bronec <71047133+Tarkann01@users.noreply.github.com>
2024-10-03 08:22:44 -0700

    Update rock-paper-scissors.py
    Adds full functionality

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -9,13 +9,23 @@ def RockPaperSiser(x):
     # rock is 3
     # siser is 2
     # paper is 1
-    # num is the computer 
+    # num is the computer
     # x is the user
     if num == x:
         return("you chose the same as the computer")
         RockPaperSiser(userInp())
     elif num == 1 and x == 3:
         return("The computer won beter luck next time")
+    elif num == 2 and x == 1:
+        return("The computer won beter luck next time")
+    elif num == 3 and x == 2:
+        return("The computer won beter luck next time")
+    elif x == 1 and num == 3:
+        return("Congrats you won!!")
+    elif x == 2 and num == 1:
+        return("Congrats you won!!")
+    elif x == 3 and  num == 2:
+        return("Congrats you won!!")
 
 def userInp():
     #Prints the three user options
@@ -26,6 +36,7 @@ def userInp():
     userChoice = int(input("What is your choice?"))
     #Returns the users choice as an int
     return userChoice
+
 
 
 
